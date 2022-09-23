0ff5061a1ad12b934229ba8264c5735d0319b175
TomBirbeck <tombirbeck47@gmail.com>
2022-09-23 13:35:13 +0100

    fixed scoring with aces

--- card-game/src/useScore.js
+++ card-game/src/useScore.js
@@ -1,39 +1,41 @@
 export default function useScore(arr) {
   let score = 0;
-  let x = 0;
-
-  arr.map((card) => {
-    switch (
-      card.value === 'KING'
-        ? (score = score + 10)
-        : card.value === 'QUEEN'
-        ? (score = score + 10)
-        : card.value === 'JACK'
-        ? (score = score + 10)
-        : card.value === 'ACE'
-        ? (score = score + x)
-        : (score = score + Number(card.value))
-    ) {
-      case 0:
-        x = 1;
-        break;
-      case score < 10:
-        x = 11;
-    }
+  let valuesArr = arr.map((arr) => {
+    return arr.value;
   });
-  //   return arr.includes('ACE') && score > 21 ? (score = score - 10) : score;
+  console.log('valuesArr', valuesArr);
+  //   let x = 0;
+
   //   arr.map((card) => {
-  //     return card.value === 'KING'
-  //       ? (score = score + 10)
-  //       : card.value === 'QUEEN'
-  //       ? (score = score + 10)
-  //       : card.value === 'JACK'
-  //       ? (score = score + 10)
-  //       : card.value === 'ACE'
-  //       ? score > 10
-  //         ? (score = score + 1)
-  //         : (score = score + 11)
-  //       : (score = score + Number(card.value));
+  //     switch (
+  //       card.value === 'KING'
+  //         ? (score = score + 10)
+  //         : card.value === 'QUEEN'
+  //         ? (score = score + 10)
+  //         : card.value === 'JACK'
+  //         ? (score = score + 10)
+  //         : card.value === 'ACE'
+  //         ? (score = score + x)
+  //         : (score = score + Number(card.value))
+  //     ) {
+  //       case 0:
+  //         x = 1;
+  //         break;
+  //       case score < 10:
+  //         x = 11;
+  //     }
   //   });
-  //   return arr.includes('ACE') && score > 21 ? (score = score - 10) : score;
+  // return arr.includes('ACE') && score > 21 ? (score = score - 10) : score;
+  arr.map((card) => {
+    return card.value === 'KING'
+      ? (score = score + 10)
+      : card.value === 'QUEEN'
+      ? (score = score + 10)
+      : card.value === 'JACK'
+      ? (score = score + 10)
+      : card.value === 'ACE'
+      ? (score = score + 11)
+      : (score = score + Number(card.value));
+  });
+  return valuesArr.includes('ACE') && score > 21 ? (score = score - 10) : score;
 }
